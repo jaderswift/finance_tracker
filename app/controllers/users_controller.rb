865fa584b38570e50ce13f3eb04696b8844803d2
jaderswift <jades@manatts.com>
2019-11-20 16:02:18 -0600

    added friend views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,29 @@ class UsersController < ApplicationController
             flash.now[:danger] = "You have not entered a search value"
         else
             @users = User.search(params[:search_param])
+            @users = current_user.except_current_user(@users)
             errorSearch = params[:search_param]
-            flash.now[:danger] = "No matches for #{ errorSearch }" unless @users.blank?
+            flash.now[:danger] = "No matches for #{ errorSearch }" if @users.blank?
         end
         respond_to do |format|
             format.js { render partial: 'friends/result' }
         end
     end
     
+    def add_friend
+        @friend = User.find(params[:friend])
+        current_user.friendships.build(friend_id: @friend.id)
+        if current_user.save
+            flash[:success] = "Friend was successfully added"
+        else
+            flash[:danger] = "There was something wrong with the friend request"
+        end
+        redirect_to my_friends_path
+    end
+
+    def show
+        @user = User.find(params[:id])
+        @user_stocks = @user.stocks
+    end
+
 end
