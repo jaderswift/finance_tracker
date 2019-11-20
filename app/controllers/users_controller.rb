68befe786806c4cacca47ba6e2ccec528d908698
jaderswift <jades@manatts.com>
2019-11-20 09:42:12 -0600

    added friend search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,20 @@ class UsersController < ApplicationController
     end
 
     def my_friends
+        @friendships = current_user.friends
+    end
 
+    def search
+        if params[:search_param].blank?
+            flash.now[:danger] = "You have not entered a search value"
+        else
+            @users = User.search(params[:search_param])
+            errorSearch = params[:search_param]
+            flash.now[:danger] = "No matches for #{ errorSearch }" unless @users.blank?
+        end
+        respond_to do |format|
+            format.js { render partial: 'friends/result' }
+        end
     end
     
 end
--- app/javascript/custom/custom.js
+++ app/javascript/custom/custom.js
@@ -5,4 +5,7 @@ $(document).on('turbolinks:load', function() {
     $('#stock-lookup-form').on('ajax:complete', function(event, data, status){                      
         $('#results').html(data)     
     })
+    $('#friend-lookup-form').on('ajax:complete', function(event, data, status){                      
+        $('#results').html(data)     
+    })
 })
